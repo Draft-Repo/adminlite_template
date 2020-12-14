c7a4f6026c509fc4473e4467d8cd912cd2162b39
Sankar Bala <sankarbala232@gmail.com>
2020-12-14 18:53:24 +0600

    Adminlite empty wrapper

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your app title</title>
+    <link href="{{ asset('css/frontend.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<p>Welcome</p>
+
+<script src="{{ asset('js/frontend.js') }}"></script>
+</body>
+</html>
